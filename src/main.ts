2853fb8d95fe51c7c1a0a527a96310f00b093235
Venkatesh <venky@applines.com>
2022-07-12 18:18:38 +0530

    included tailwind css

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import App from './App.vue'
 import router from './router'
 
 import 'bootstrap/dist/css/bootstrap.css';
+import '@/assets/tailwind.css';
+import '@/scss/index.scss';
 import pinia from './config/pinia-setup';
 import i18n from './config/i18n-config';
 import primeVue from './config/prime-vue-config';
